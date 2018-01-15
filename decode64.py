864a29b337c289eccbd04cb87166f555c4487c5c
Mayara Ribeiro Fernandes <ammmayara@hotmail.com>
2018-01-15 11:17:55 -0200

    Exemplo código em Python pra library customizada
    Exemplo código em Python que decodifica uma string em base64

---
+++ decode64.py
@@ -0,0 +1,11 @@
+import base64
+
+def decode_64_to_string(stringEncoded):
+    print("Deconding [%s] to string." % (stringEncoded,))
+
+    if not stringEncoded.endswith("=="):
+        stringEncoded = stringEncoded + "=="
+
+    stringDecoded = base64.b64decode(stringEncoded)
+
+    return stringDecoded.decode('UTF-8')
